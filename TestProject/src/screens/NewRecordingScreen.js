3a6d950765dd477f48d8819b439e082a3f8b4e82
Ian Oxborrow <u6668026@anu.edu.au>
2021-04-13 01:11:14 +1000

    enabled item to be removed from the sensor list

--- TestProject/src/screens/NewRecordingScreen.js
+++ TestProject/src/screens/NewRecordingScreen.js
@@ -1,5 +1,5 @@
 import 'react-native-gesture-handler';
-import React, { Component, useState } from 'react';
+import React, { Component, useState, useRef} from 'react';
 //import { IconButton, Colors } from 'react-native-paper';
 import { FloatingAction } from "react-native-floating-action";
 //import { FAB } from 'react-native-paper';
@@ -25,7 +25,7 @@ class NewRecordingScreen extends Component {
     super(props);
     //this.props = props
     this.state = {
-      currentSensorSelection: "",
+      currentSensorSelection: "acceleromoter",
       currentSampleRate: "",
       currentLabelAddition: "",
       selectedSensorData: [],
@@ -33,11 +33,14 @@ class NewRecordingScreen extends Component {
       addedLabels: []
     }
 
-    this.usedSensors = {}
+    this.sensorPicker;
+    
+    this.usedSensors = {'acceleromoter': false, 'gyroscope': false, 'microphone': false}
   }
 
   componentDidMount = () => {
-  
+    //make sure that the accelorometer is the default item in the picker view
+    //this.sensorPicker.selectItem('acceleromoter');
   }
 
   sensorListHeader = () => {
@@ -50,7 +53,45 @@ class NewRecordingScreen extends Component {
       
       <Text>{item.sampleRate}</Text>
       
-      <Image source={require("../assets/baseline_close_black.png")} style={{marginLeft: 'auto'}}/>
+      <TouchableOpacity 
+          style={{marginLeft: 'auto'}} 
+          onPress={() => {  
+            console.log('waspressed' + item.sensorName)
+            //use the sensor name to identify which row was pressed to work out which data to remove
+
+            //make the sensor as not being used
+            this.usedSensors[item.sensorName] = false
+
+            //remove the sensor from the sensordata array
+            console.log('befroe' + this.state.selectedSensorData)
+            for (var i in this.state.selectedSensorData) {
+              var sensorData = this.state.selectedSensorData[i]
+              if (item.sensorName == sensorData['sensorName']){
+                this.state.selectedSensorData.splice(i,1);
+                break;
+              }
+            }
+
+            console.log('after' + this.state.selectedSensorData)
+          
+            //
+            this.setState({selectedSensors: [...this.state.selectedSensorData] })
+            
+            //set the new default vaule to be the first non hidden value
+            for (key in this.usedSensors) {
+              if (!this.usedSensors[key]) {
+                this.sensorPicker.selectItem(key);
+                break
+              }
+
+              //if all the sensors have been selected, set the curret selection to the 'empty' value
+              this.sensorPicker.selectItem('empty')
+            }
+
+          }}>
+            
+            <Image source={require("../assets/baseline_close_black.png")} style={{marginLeft: 'auto'}}/>
+        </TouchableOpacity>
     </View>
   );
 
@@ -60,15 +101,19 @@ class NewRecordingScreen extends Component {
       <View style={{flexDirection: 'row', alignItems: 'center', flex: 1}}>
         <Text style={styles.title}>{""}</Text>
         <DropDownPicker
+          ref={this.sensorPickerRef}
           items={[
-            {label: 'Acceleromoter', value: 'acceleromoter', hidden: false},
-            {label: 'Gyroscope', value: 'gyroscope' },
-            {label: 'Microphone', value: 'microphone'},
+            {label: 'Acceleromoter', value: 'acceleromoter', hidden: this.usedSensors['acceleromoter']},
+            {label: 'Gyroscope', value: 'gyroscope', hidden: this.usedSensors['gyroscope']},
+            {label: 'Microphone', value: 'microphone', hidden: this.usedSensors['microphone']},
+            {label: '', value: 'empty', hidden: true},
               /*{label: 'Acceleromoter', value: 'acceleromoter', icon: () => <Icon name="flag" size={18} color="#900" />, hidden: true},
               {label: 'Gyroscope', value: 'gyroscope', icon: () => <Icon name="flag" size={18} color="#900" />},
               {label: 'Microphone', value: 'microphone', icon: () => <Icon name="flag" size={18} color="#900" />},*/
           ]}
           defaultValue={'acceleromoter'}
+          controller={instance => this.sensorPicker = instance}
+
           containerStyle={{height: 40, width: 150}}
           style={{backgroundColor: '#fafafa'}}
           itemStyle={{
@@ -92,13 +137,31 @@ class NewRecordingScreen extends Component {
         <TouchableOpacity 
           style={{marginLeft: 'auto'}} 
           onPress={() => {  
-            //update the flatlist with the new information
-            newSensor = {sensorName: this.state.currentSensorSelection, 
-                         sampleRate: this.state.currentSampleRate}
-            this.state.selectedSensorData.push(newSensor)
-            this.setState({selectedSensors: [...this.state.selectedSensorData] })
-            
-            this.sampleRateInput.clear()
+
+            //make sure that a value has been entered into the lable name textinput before the button is allowed to be pressed
+            if (this.state.currentSensorSelection != "empty" && this.state.currentSampleRate != "") {
+              newSensor = {sensorName: this.state.currentSensorSelection, 
+                          sampleRate: this.state.currentSampleRate}
+
+              //add the selected sensor to a dictioany to mark that it has been selected
+              this.usedSensors[this.state.currentSensorSelection] = true
+
+              this.state.selectedSensorData.push(newSensor)
+              this.setState({selectedSensors: [...this.state.selectedSensorData] })
+              
+              //set the new default vaule to be the first non hidden value
+              for (key in this.usedSensors) {
+                if (!this.usedSensors[key]) {
+                  this.sensorPicker.selectItem(key);
+                  break
+                }
+
+                //if all the sensors have been selected, set the curret selection to the 'empty' value
+                this.sensorPicker.selectItem('empty')
+              }
+
+              this.sampleRateInput.clear()
+            }
           }} 
           >
           <Image source={require("../assets/baseline_add_black.png")} />
@@ -131,12 +194,16 @@ class NewRecordingScreen extends Component {
         <TouchableOpacity 
           style={{marginLeft: 'auto'}} 
           onPress={() => {  
-            //update the flatlist with the new information
-            newLabel = {labelName: this.state.currentLabelAddition}
-            this.state.addedLabels.push(newLabel)
-            this.setState({addedLabels: [...this.state.addedLabels] })
             
-            this.labelNameInput.clear()
+            //make sure that a value has been entered into the lable name textinput before the button is allowed to be pressed
+            if (this.state.currentLabelAddition != "") {
+
+              newLabel = {labelName: this.state.currentLabelAddition}
+              this.state.addedLabels.push(newLabel)
+              this.setState({addedLabels: [...this.state.addedLabels] })
+              
+              this.labelNameInput.clear()
+            }
           }} >
           <Image source={require("../assets/baseline_add_black.png")} style={{marginLeft: 'auto'}}/>
         </TouchableOpacity>
@@ -166,7 +233,7 @@ class NewRecordingScreen extends Component {
         <FlatList
           data={this.state.selectedSensors}
           renderItem={this.sensorListItem}
-          keyExtractor={item => item} 
+          keyExtractor={item => item.sensorName} 
           ListFooterComponent = {this.sensorListFooter}/>
 
         <View>
