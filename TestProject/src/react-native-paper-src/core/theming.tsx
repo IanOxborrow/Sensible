2a142d5c43666df03b40002447436d1c384603ff
Ian Oxborrow <u6668026@anu.edu.au>
2021-04-28 00:56:51 +1000

    added screen recording buton

---
+++ TestProject/src/react-native-paper-src/core/theming.tsx
@@ -0,0 +1,7 @@
+//import { createTheming } from '@callstack/react-theme-provider';
+import { createTheming } from '../../react-theme-provider-master-src'
+import DefaultTheme from '../styles/DefaultTheme';
+
+export const { ThemeProvider, withTheme, useTheme } = createTheming<
+  ReactNativePaper.Theme
+>(DefaultTheme as ReactNativePaper.Theme);
