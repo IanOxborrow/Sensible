d5c3a3f3529cb59ca1df415270b40bab4cb12616
Ian Oxborrow <u6668026@anu.edu.au>
2021-09-20 03:42:07 +1000

    added gps icon

--- TestProject/src/Sensors.js
+++ TestProject/src/Sensors.js
@@ -107,13 +107,13 @@ export const SensorInfo = {
     [SensorType.GPS]: {
         name: "GPS",
         type: HardwareType.SENSOR,
-        imageSrc: require('./assets/baseline_close_black.png'),  // TODO: Update icon!
+        imageSrc: require('./assets/gps_icon.png'),  // TODO: Update icon!
         class: GPS,
         measure: "Coordinates",
         units: "°",
         description: {
             measure: "Your current location on the globe",
-            output: "X and Y per sample rate, representing current global position"
+            output: "lat and long per sample rate, representing current global position"
         }
     },
     [SensorType.BACK_CAMERA]: {
@@ -125,8 +125,8 @@ export const SensorInfo = {
         measure: "Video",
         units: "",
         description: {
-            measure: "ADD MEASURE HERE",
-            output: "ADD OUTPUT HERE"
+            measure: "Video feed of the back camera",
+            output: "MP4 file, saved to device"
         }
     }
 }
