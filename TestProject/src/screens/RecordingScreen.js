c6b40a0498dcd86835c52705a34cff401203981f
Ryan Turner <u6040885@anu.edu.au>
2021-10-09 13:39:22 +1100

    #184 UI indicators added

--- TestProject/src/screens/RecordingScreen.js
+++ TestProject/src/screens/RecordingScreen.js
@@ -112,7 +112,6 @@
        * Toggles the display of the indicators
        */
      toggleIndicators() {
-        console.log("Toggling")
         this.setState({indicatorStatus: !this.state.indicatorStatus})
      }
 
